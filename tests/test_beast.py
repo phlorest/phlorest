1275f75419efa6ba9c26e6ad0093b310fc40dea3
Simon J Greenhill <SimonGreenhill@users.noreply.github.com>
2022-05-09 18:33:39 +1200

    speed up very slow beast test (#27)

--- tests/test_beast.py
+++ tests/test_beast.py
@@ -1,10 +1,8 @@
-import pytest
-
 from phlorest import BeastFile
 
-
-@pytest.mark.slow
 def test_BeastFile(dataset):
     bf = BeastFile(None, text=dataset.raw_dir.read('beast2.xml.gz'))
     nex, chars = bf.nexus_and_characters()
-    assert len(chars) == 18438
+    assert chars[135] == (136, 'again-62')  # check one
+    assert sorted(nex.data.taxa) == sorted(['A', 'B', 'C'])
+    assert len(chars) == nex.data.nchar == 235
