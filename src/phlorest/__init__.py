8d6839119625fa5076fbad88e52c2ca63fadeb30
Robert Forkel <xrotwang@googlemail.com>
2023-07-26 11:26:23 +0200

    bump version for development

--- src/phlorest/__init__.py
+++ src/phlorest/__init__.py
@@ -5,7 +5,7 @@ from .beast import BeastFile
 from .nexuslib import NexusFile
 from . import commands
 
-__version__ = '1.2.0'
+__version__ = '1.2.1.dev0'
 __all__ = ['Dataset', 'Metadata', 'BeastFile', 'NexusFile', 'CLDFWriter']
 
 assert commands
