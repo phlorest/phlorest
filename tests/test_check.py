140587d2f51866de49feaeda0e7c33656a706e76
Robert Forkel <xrotwang@googlemail.com>
2023-11-16 16:46:06 +0100

    make sure we distribute the check.R script

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,8 +1,11 @@
 import logging
+import pathlib
 
+import phlorest
 from phlorest.check import run_checks
 
 
 def test_run_checks(dataset, caplog):
     assert run_checks(dataset.cldf_reader(), logging.getLogger(__name__)) is False
     assert len(caplog.records) == 5
+    assert pathlib.Path(phlorest.__file__).parent.joinpath('check.R').exists()
