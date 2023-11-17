9aa27d495bf7129c6ec4ffc369bed5ccb5e5d1ec
Robert Forkel <xrotwang@googlemail.com>
2023-11-17 12:23:04 +0100

    release 1.6.0

--- src/phlorest/__init__.py
+++ src/phlorest/__init__.py
@@ -5,7 +5,7 @@ from .beast import BeastFile
 from .nexuslib import NexusFile
 from . import commands
 
-__version__ = '1.5.0'
+__version__ = '1.6.0'
 __all__ = ['Dataset', 'Metadata', 'BeastFile', 'NexusFile', 'CLDFWriter']
 
 assert commands
